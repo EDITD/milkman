717a77b1ad8f9339fa077e144c08f74a152350c3
Nick Barrett <nick@edited.com>
2018-05-02 13:34:35 +0100

    Remove long_description rubbish.

--- setup.py
+++ setup.py
@@ -3,13 +3,6 @@ import sys
 from setuptools import setup, find_packages
 
 milkman = __import__("milkman")
-readme_file = os.path.join(os.path.dirname(__file__), "docs/index.txt")
-try:
-    long_description = open(readme_file).read()
-except IOError as err:
-    sys.stderr.write("[ERROR] Cannot find file specified as "
-        "``long_description`` (%s)\n" % readme_file)
-    sys.exit(1)
 
 if sys.version_info >= (3,):
     extra_kwargs = {"use_2to3": True}
@@ -20,7 +13,6 @@ setup(
     name="milkman",
     version=milkman.get_version(),
     description="Testing Django without all the fixtures",
-    long_description=long_description,
     author="Wilkes Joiner, Chuck Collins, Patrick Altman",
     author_email="chuck.collins@gmail.com",
     license="BSD",
